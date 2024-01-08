2057e2b10f6bb67e5acdde607e75476079e5ac37
pyroby <r.schiavelli@ilivetech.it>
2024-01-08 14:08:07 +0100

    aggiunto logo e foto profilo + dockerfile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
 import { AppBar, Button, ButtonGroup, Toolbar } from '@mui/material';
 import React from 'react'
-import { FaCode } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <React.Fragment>
         <AppBar sx={{ width:'100%'}}>
             <Toolbar sx={{ display:'flex', justifyContent:'space-around'}}>
-                <FaCode size={30} />
+                <img src='/LogoPyRoby_1_-removebg-preview.png' alt='logo' />
                 <ButtonGroup variant='outlined'>
                     <Button sx={{ fontWeight:'bold'}} >Home</Button>
                     <Button sx={{ fontWeight:'bold'}} >About</Button>
